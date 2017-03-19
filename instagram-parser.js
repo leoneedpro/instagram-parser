eb5dd6149e12ab6db0e746a3b9e656576c31bd08
Леонид Залюбовский <iam@leoneed.pro>
2017-03-20 08:59:10 +1000

    скрипт сбора данных

---
+++ instagram-parser.js
@@ -0,0 +1,72 @@
+// Copyright Леонид Залюбовский 2017
+try {
+// Объявление переменных ------------------------------------------------------------
+var a = document.getElementsByClassName("_539vh _4j13h");   // Instagram
+var x = document.getElementsByClassName("_4gt3b");                // Instagram
+var n = document.getElementsByClassName("_cx1ua");               // Instagram
+var timeoutID ='';
+var t = 1;
+
+// Выборка кол-ва подписчиков и подписок по языку RU-EN ---------
+
+if (document.getElementsByClassName("_q44m8 _7z4zb")[0].innerHTML =="Подписчики" || document.getElementsByClassName("_q44m8 _7z4zb")[0].innerHTML =="Followers" ){
+var i = document.getElementsByClassName("_bkw5z")[1].innerHTML;
+}else{
+var i = document.getElementsByClassName("_bkw5z")[2].innerHTML;
+}
+
+
+// Функция  --------------------------------------------------------------------------------------------
+
+i = i.match(/[^"]+/g);
+i = i.join('');
+i = i.match(/[^\s]+/g);
+i = i.join('');
+i = i.match(/[^,]+/g);
+i = i.join('');
+console.log('%cПАРСЕР ПОДПИСЧИКОВ INSTAGRAM', 'color: #1d6ba3; font-size:28px;');
+console.log('%c--------------------------------------------------------------------------------------------//', 'color: #a22e1c; font-size:16px;');
+console.log('%cРазработал Леонид Залюбовский 2017 | www.leoneed.pro | http://Instagram.com/leoneed.pro', 'color: #1d6ba3; font-size:14px;');
+console.log('%c--------------------------------------------------------------------------------------------//', 'color: #a22e1c; font-size:16px;');
+console.log('%cКол-во аккаунтов для сбора: '+i +' шт.', 'color: #13a555; font-size:20px;');
+console.log('%cНачался сбор данных, дождитесь выполнения...', 'color: #13a555; font-size:20px;');
+function start() {
+  t++;
+if (i == t){
+off();
+  console.log('%cАккаунтов собрано: '+n.length +' шт.', 'color: #13a555; font-size:20px;');
+  if (i>n.length){console.log('%cСтранно, но '+(i - n.length) +' шт. не определились, ну и ладно=)', 'color: #13a555; font-size:20px;')};
+  var text =  a[0].innerHTML;
+  var result = text.match(/title="[^"]+"/g);
+  result = result.join(' ');
+  result = result.match(/"[^"]+"/g);
+  result = result.join(' ');
+  result = result.match(/[^"]+/g);
+  result = result.join('');
+  result = result.match(/[^\s]+/g);
+  result = result.join('\n');
+  var followers ={};
+  followers.name = result;
+console.log('%c//--------------------------------------------------------------------------------------------', 'color: #a22e1c; font-size:16px;');
+  console.log(result);
+console.log('%c//--------------------------------------------------------------------------------------------', 'color: #a22e1c; font-size:16px;');
+  console.log('%cПоздравляю! Выделите собранные имена аккаунтов выше и нажмите CTRL-C, чтобы скопировать. Перейдите в любой текстовый редактор и нажмите CTRL-V, чтобы вставить данные.', 'color: #13a555; font-size:24px;');
+console.log('%c//--------------------------------------------------------------------------------------------', 'color: #13a555; font-size:16px;');
+  console.log('%cЗаходите подписывайтесь! Ставьте лайки! =) http://Instagram.com/leoneed.pro | www.leoneed.pro ', 'color: #1d6ba3; font-size:14px;');
+}else{
+x[0].scrollTop = x[0].scrollHeight;
+run()
+}
+}
+
+function run() {
+ timeoutID = setTimeout(start, 100);
+}
+start();
+}catch(e){
+console.log('%cНажмите на странице Instagram на Подписчиков или Подписки, и запустите заново скрипт)', 'color: #a22e1c; font-size:28px;');
+}
+function off() {
+    clearTimeout(timeoutID);
+    timeoutID = null;
+}
